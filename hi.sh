6fb24ac5218e0f8c04c8e29099b89943aa9b8828
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-05-05 14:16:43 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -1,1 +1,2 @@
-echo "You've executed the shell!"
+curl -s https://raw.githubusercontent.com/justin-a30/aod_setup/main/apks/aod/hos1.zip /sdcard/test/temp/hos1.zip
+unzip /sdcard/test/temp/hos1.zip /sdcard/test/file/aod/hos1
