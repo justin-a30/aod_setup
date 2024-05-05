ed95fb674598f7a2732258607e5185140219a221
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-05-05 14:19:26 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -1,2 +1,3 @@
-curl https://raw.githubusercontent.com/justin-a30/aod_setup/main/apks/aod/hos1.zip /sdcard/test/temp/hos1.zip
+mkdir /sdcard/test/temp
+curl -s https://raw.githubusercontent.com/justin-a30/aod_setup/main/apks/aod/hos1.zip /sdcard/test/temp/hos1.zip
 unzip /sdcard/test/temp/hos1.zip /sdcard/test/file/aod/hos1
