1a41e291db03a5e916cae8bc2ec5cb8786f4a488
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-05-05 14:34:38 +0700

    Update hi.sh

--- hi.sh
+++ hi.sh
@@ -1,3 +1,5 @@
 mkdir /sdcard/test/temp
+mkdir /sdcard/test/file
+mkdir /sdcard/test/file/aod
 curl -s https://raw.githubusercontent.com/justin-a30/aod_setup/main/apks/aod/hos1.zip --output /sdcard/test/temp/hos1.zip
 unzip /sdcard/test/temp/hos1.zip -d /sdcard/test/file/aod/hos1
