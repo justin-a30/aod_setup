343fd61f647c327b5455315124d2bef4338f2362
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-05-10 17:46:25 +0700

    mo 2.0 released

--- shell/mo.sh
+++ shell/mo.sh
@@ -14,3 +14,16 @@ multi_option() {
 # FOR NOW, EVERYTIME YOU MENTION "multi_option"
 # MODIFIED BINARY WILL RUN, INSTEAD OF ORIGINAL ONE.
 
+multi_option2() {
+   local end="$2" i=0 loop=false
+   [ -z "$1" ] && return 0
+   [[ "$3" == "loop" ]] && loop=true 
+ while true; do
+    i=$(($i+1))
+    if $yes; then ui_print "${n} [i] Selected $i" && setdefault "$1" $i && return $i
+    else ui_print " [>> Option: $(($i+1))"
+    fi
+    if is_number $end && is_greater_equal $i $end; then if $loop; then i=0; echo " "; continue; else setdefault "$1" ""; return 0; fi; fi
+    if is_greater $i $end; then setdefault "$1" ""; return 0; fi
+ done
+}
