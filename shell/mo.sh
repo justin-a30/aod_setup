324dbb5a84f9becb641acada39bd65f7e0ba7f5e
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-05-11 11:30:45 +0700

    idkidk

--- shell/mo.sh
+++ shell/mo.sh
@@ -3,21 +3,6 @@ multi_option() {
    local end="$2" i=0 loop=false
    [ -z "$1" ] && return 0
    [[ "$3" == "loop" ]] && loop=true 
-   while true; do
-      i=$(($i+1))
-      if $yes; then ui_print "${n} [i] Selected $i" && setdefault "$1" $i && return $i
-      else ui_print " [>> Option: $(($i+1))"
-      fi
-      if is_number $end && is_greater_equal $i $end; then if $loop; then i=0; echo " "; continue; else setdefault "$1" ""; return 0; fi; fi
-   done
-}
-# FOR NOW, EVERYTIME YOU MENTION "multi_option"
-# MODIFIED BINARY WILL RUN, INSTEAD OF ORIGINAL ONE.
-
-multi_option2() {
-   local end="$2" i=0 loop=false
-   [ -z "$1" ] && return 0
-   [[ "$3" == "loop" ]] && loop=true 
  while true; do
     i=$(($i+1))
     if $yes; then ui_print "${n} [i] Selected $i" && setdefault "$1" $i && return $i
@@ -27,3 +12,18 @@ multi_option2() {
     if is_greater $i $end; then setdefault "$1" ""; return 0; fi
  done
 }
+
+# multi_option() {
+#    local end="$2" i=0 loop=false
+#    [ -z "$1" ] && return 0
+#    [[ "$3" == "loop" ]] && loop=true 
+#    while true; do
+#       i=$(($i+1))
+#       if $yes; then ui_print "${n} [i] Selected $i" && setdefault "$1" $i && return $i
+#       else ui_print " [>> Option: $(($i+1))"
+#       fi
+#       if is_number $end && is_greater_equal $i $end; then if $loop; then i=0; echo " "; continue; else setdefault "$1" ""; return 0; fi; fi
+#    done
+# }
+# FOR NOW, EVERYTIME YOU MENTION "multi_option"
+# MODIFIED BINARY WILL RUN, INSTEAD OF ORIGINAL ONE.
