8de0fdf9080731f5a162edd2450c762e68590cda
justin-a30 <51263008+justin-a30@users.noreply.github.com>
2024-06-06 11:08:38 +0700

    devicelevellist fixup

--- shell/install.sh
+++ shell/install.sh
@@ -270,9 +270,9 @@ ui_print " [----------] [Getting ready...]"
                             ui_print " [#---------] [Placing High End props...]"
                             touch $MODPATH/system.prop
                             add_lines_string "ro.config.low_ram=false" "ro.config.low_ram.threshold_gb=0" "ro.miui.backdrop_sampling_enabled=true" "ro.miui.has_real_blur=1" "ro.miui.has_blur=1"  "ro.miui.has_handy_mode_sf=1"  "ro.launcher.blur.appLaunch=1" "ro.surface_flinger.supports_background_blur=1" "ro.sf.blurs_are_expensive=1" "persist.sys.sf.disable_blurs=false" "enable_blurs_on_windows=1" "ro.sf.blurs_are_caro=0" "persist.sys.background_blur_supported=true" "vendor.perf.framepacing.enable=false" "persist.sys.power.default.powermode=1" "ro.vendor.sf.detect.aod.enable=true" $MODPATH/system.prop
-                            settings put system deviceLevelList "v:1;c:3;g:3"
+                            settings put system deviceLevelList "v:1,c:3,g:3"
                             touch $MODPATH/post-fs-data.sh
-                            add_lines_string 'MODDIR=${0%/*}' 'settings put system deviceLevelList "v:1;c:3;g:3"' $MODPATH/post-fs-data.sh
+                            add_lines_string 'MODDIR=${0%/*}' 'settings put system deviceLevelList "v:1,c:3,g:3"' $MODPATH/post-fs-data.sh
                     else
                         ui_print " "
                         ui_print " [#---------] [Skipping HighEnd props]"
