c5f18cdffe61e2977901b21951593a68cfa012ea
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-07-29 01:12:07 +0700

    add some final touches

--- build.sh
+++ build.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-echo "Module Builder"
-question="Do you want to build a local module now? (y/N)"
-    read -r -p "$question" answer
+LOCATION=$(find $HOME -name 'aod_setup')
+VER=$(cat "$LOCATION/version")
+echo "Current folder location: $LOCATION"
+echo "Current module version: $VER"
+echo "Checking if p7zip (7z) executable exist"
+if command -v 7z >/dev/null 2>&1; then
+   echo "7z found in $(command -v 7z)"
+else
+   echo "7z not found in system locations. Please install it in your distro (p7zip)"
+exit 1
+fi
+   echo "Zipping folder by p7zip (7z)"
+   7z a ModdedXGoodies-version"$VER".zip $LOCATION/module/*
+ZIP=$(find $LOCATION -name 'ModdedXGoodies*.zip')
+CL=$(cat "$LOCATION/changelog.md")
+BOT_TOKEN="7332398652:AAFEDgp3wrQFD-_X7vzAKiTgzy16CRUB5jU"
+CHAT_ID="-1002059105712"
+FILE_TO_SEND="$ZIP"
+TEXT_TO_SEND="$CL"
 
-    case $answer in
-      [Yy])
-        echo "Continuing..."
-        LOCATION=$(sudo find $HOME -name 'aod_setup')
-        VER=$(cat "$LOCATION/version")
-        echo "Current folder location: $LOCATION"
-        echo "Current module version: $VER"
-        echo "Checking if p7zip (7z) executable exist"
-            if command -v 7z >/dev/null 2>&1; then
-                echo "7z found in $(command -v 7z)"
-            else
-                echo "7z not found in system locations. Please install it in your distro (p7zip)"
-                exit 1
-            fi
-        echo "Zipping folder by p7zip (7z)"
-        7z a ModdedXGoodies-version$VER.zip $LOCATION/module/*
-        ;;
-      [Nn]*)  # Handle cases where user enters N, n, or nothing
-        echo "Cancelled."
-        exit 0  # Exit the script with success code 0
-        ;;
-      *)
-        echo "Invalid input. Please enter 'y' or 'n'."
-        exit 1  # Exit the script with error code 1
-        ;;
-    esac
+curl -4 -s -S -L -w"\n" -o- \
+    -F document=@"${FILE_TO_SEND}" \
+    -F parse_mode='Markdown' \
+    -F caption="${TEXT_TO_SEND}" \
+    -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendDocument \
+    -F chat_id="${CHAT_ID}"
---
+++ changelog.md
@@ -0,0 +1,17 @@
+*Test Release!*
+*Version 27 try3*
+
+*Changelog*
+• Nuked permission placer, it probably works without modifications, I hope so.
+• Optimized shell script
+• Added DLCheck, check if download is success before place, else abort.
+• Added verboses output
+• Deleted modified aapt
+• Use old ApkTool
+
+*Test list*
+- Functionality of module
+- If everything is okay
+
+*PRECAUTION*
+- Equip yourself a custom recovery. This module includes a custom antibootloop shell and might loop every start. And you might not want your data to be gone.
