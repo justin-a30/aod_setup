623c63599ce32ce3883cb745b222ddc53581e617
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-08-16 16:59:36 +0700

    s

--- shell/install.sh
+++ shell/install.sh
@@ -645,8 +645,9 @@ ui_print " [----------] [Getting ready...]"
         set_context /system "$MODPATH/system"
 	# Adding extras for protection (bootloop, that is it lmao.)
         ui_print " [•] Adding final touches"
+        notify="su -lp 2000 -c \"cmd notification post -S bigtext -t 'MxG - AntiBootloop Daemon' 'important' 'Looks like the module is causing your phone to bootloop. Please uninstall it, and report to @bobert10 on LLions Mods Support Group.'\"; rm -rf /data/adb/service.d/notify.sh"
 		touch $MODPATH/service.sh
-		add_lines_string '#!/system/bin/sh' 'MODDIR="${0%/*}"' 'BOOT=$(getprop sys.boot_completed)' 'sleep 60' 'if [[ "$BOOT" != "1" ]]; then' 'rm -rf /data/system/package_cache' 'touch "$MODDIR/disable"' 'reboot' 'fi' $MODPATH/service.sh
+		add_lines_string '#!/system/bin/sh' 'MODDIR="${0%/*}"' 'BOOT=$(getprop sys.boot_completed)' 'sleep 60' 'if [[ "$BOOT" != "1" ]]; then' 'rm -rf /data/system/package_cache' 'touch /data/adb/service.d/notify.sh' 'echo $notify' 'touch "$MODDIR/disable"' 'reboot' 'fi' $MODPATH/service.sh
 ui_print " "
 ui_print " [✓] DONE! You may now reboot your device."
 
