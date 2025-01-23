6cc1cb661f0861d1f6cdea5a2ca615d3fe4a4136
justin-a30 <51263008+justin-a30@users.noreply.github.com>
2025-01-23 17:59:58 +0700

    Add module existence check and update version to 60

--- META-INF/com/google/android/magisk/customize.sh
+++ META-INF/com/google/android/magisk/customize.sh
@@ -15,7 +15,15 @@
         DevName=$(getprop ro.product.odm.device)
         OS=$(getprop ro.build.version.incremental)
         MAJOR_VERSION=$(echo "$OS" | grep -o '^[0-9]\+')
+        MODULE_EXISTENCE="/data/adb/modules/moddedxgoodies"
 
+# ExtraCheck(TM)
+    if [ -d "$MODULE_EXISTENCE" ]; then
+        ui_print " [!] ModdedXGoodies is already installed."
+        ui_print "     Please uninstall the module first."
+        ui_print "     If you want to update, please uninstall, reboot,"
+           abort "     and install the latest version."
+    fi
 # PRINT NOTES
     ui_print " --## NOTES ##--"
     ui_print " Installation TUI has been improved."
